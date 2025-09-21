77aa05be41c410ed3110ac706eb1da630be2bddc
SirHellcat <framdorv@unal.edu.co>
2025-09-20 22:04:35 -0400

    Added the function to send a POST request to Watson NLP Library and et the results of the emotion detector

---
+++ emotion_detection.py
@@ -0,0 +1,23 @@
+# we need to import requests to send the request to the Watson NLP Library
+import requests
+# we need to import json to be able to send the text to analyze
+import json
+
+def emotion_detector(text_to_analyse):
+    # URL of the Watson NLP service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Custom header specifying the model ID for the sentiment analysis service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Sending a POST request to the sentiment analysis API
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Return the response
+    return response.text
